11ff50513188edc2fef35e52cb43663fae54987b
ahsan-khan1999 <ahsan.ak943@gmail.com>
2019-09-17 10:31:37 -0700

    Basics Stuff

---
+++ App.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import greetings from './Component/Hello' 
+// import Component from React
+
+class App extends Component{
+    render(){
+        return(
+           <div className = "App">
+           <greetings />
+           </div> 
+
+        );
+
+
+    }
+
+
+}
+
+
+
+
+export default App;
---
+++ Component/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function greetings(){
+
+    return <h1>x</h1>
+    
+}
+
+export default greetings;
